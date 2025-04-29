a90f00093fc5c4cb192da05ef223cf85af2bfaed
Varunvaru27 <varunvg2001@gmail.com.com>
2025-04-29 11:13:13 +0530

    second

--- project/urls.py
+++ project/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/',include('app.urls')),
+    
     path('about/',views.about),
     path('contact/',views.contact),
-    path('home/',views.home)
+    path('home/',views.home),
+    path("home_car/",views.car1),
+    path("car2/<int:id>",views.car2,name='kumar')
 ]
